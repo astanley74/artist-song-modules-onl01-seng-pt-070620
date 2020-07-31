84c7102d49184c45909db47e9d2badffc920ed56
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-31 03:22:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,9 +19,12 @@ class Artist
     @@artists
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-
 end
